680d28d90a0ee0b64dbfe30c128ca42485fe8c6d
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-02-09 13:47:26 -0800

    calculator instantiates correctly

--- spec/planet-age-project-spec.js
+++ spec/planet-age-project-spec.js
@@ -1,9 +1,9 @@
-// import { Calculator } from "./../js/scripts.js";
-//
-// describe('Calculator', function(){
-//
-//   it ('should instantiate Calculator correctly', function(){
-//     let newCalc = new Calculator("06-15-1987");
-//     expect(newCalc instanceof Calculator);
-//   })
-// })
+import { Calculator } from "./../js/scripts.js";
+
+describe('Calculator', function(){
+
+  it ('should instantiate Calculator correctly', function(){
+    let newCalc = new Calculator("06-15-1987");
+    expect(newCalc instanceof Calculator);
+  })
+})
