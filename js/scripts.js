beff20d4a7f559e3a25351ca02f626504c10aff7
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-02-09 16:09:59 -0800

    pass all tests

--- js/scripts.js
+++ js/scripts.js
@@ -5,63 +5,67 @@ export class Calculator {
   }
 
   calculateAgeMercury() {
-    console.log(this.age);
     let ms = new Date(this.age);
-    console.log(ms);
-    let ageNumber = ms.getTime();
-    let mercuryNumber = parseFloat(ageNumber) / .24;
-    let mercuryYear = mercuryNumber / 31536000000;
+    let currentDate = new Date();
+    let ageNumber = currentDate.getTime() - ms.getTime();
+    console.log(ageNumber / 31557600000);
+    let mercuryNumber = parseFloat(ageNumber) / 0.24;
+    let mercuryYear = mercuryNumber / 31557600000;
+    console.log("Mercury: " + mercuryYear);
     return mercuryYear;
   }
 
   calculateMercuryLifeExpectancy(expectancy) {
     let age = this.calculateAgeMercury();
-    let expectancyYear = parseFloat(expectancy) / .24;
+    let expectancyYear = parseFloat(expectancy) / 0.24;
     let yearsLeft = expectancyYear - age;
+    console.log("Mercury: " + yearsLeft);
     return yearsLeft;
   }
 
   calculateAgeVenus() {
-    console.log(this.age);
     let ms = new Date(this.age);
-    console.log(ms);
-    let ageNumber = ms.getTime();
-    let venusNumber = parseFloat(ageNumber) / .62;
-    let venusYear = venusNumber / 31536000000;
+    let currentDate = new Date();
+    let ageNumber = currentDate.getTime() - ms.getTime();
+    let venusNumber = parseFloat(ageNumber) / 0.62;
+    let venusYear = venusNumber / 31557600000;
+    console.log("Venus: " + venusYear);
     return venusYear;
   }
 
   calculateVenusLifeExpectancy(expectancy) {
     let age = this.calculateAgeVenus();
-    let expectancyYear = parseFloat(expectancy) / .62;
+    let expectancyYear = parseFloat(expectancy) / 0.62;
     let yearsLeft = expectancyYear - age;
+    console.log("Venus: " + yearsLeft);
     return yearsLeft;
   }
 
   calculateAgeMars() {
-    console.log(this.age);
     let ms = new Date(this.age);
-    console.log(ms);
-    let ageNumber = ms.getTime();
+    let currentDate = new Date();
+    let ageNumber = currentDate.getTime() - ms.getTime();
     let marsNumber = parseFloat(ageNumber) / 1.88;
-    let marsYear = marsNumber / 31536000000;
+    let marsYear = marsNumber / 31557600000;
+    console.log("Mars: " + marsYear);
     return marsYear;
   }
 
   calculateMarsLifeExpectancy(expectancy) {
-    let age = this.calculateAgeMercury();
+    let age = this.calculateAgeMars();
     let expectancyYear = parseFloat(expectancy) / 1.88;
     let yearsLeft = expectancyYear - age;
+    console.log("Mars: " + yearsLeft);
     return yearsLeft;
   }
 
   calculateAgeJupiter() {
-    console.log(this.age);
     let ms = new Date(this.age);
-    console.log(ms);
-    let ageNumber = ms.getTime();
+    let currentDate = new Date();
+    let ageNumber = currentDate.getTime() - ms.getTime();
     let jupiterNumber = parseFloat(ageNumber) / 11.86;
-    let jupiterYear = jupiterNumber / 31536000000;
+    let jupiterYear = jupiterNumber / 31557600000;
+    console.log("Jupiter: " + jupiterYear);
     return jupiterYear;
   }
 
@@ -69,6 +73,7 @@ export class Calculator {
     let age = this.calculateAgeJupiter();
     let expectancyYear = parseFloat(expectancy) / 11.86;
     let yearsLeft = expectancyYear - age;
+    console.log("Jupiter: " + yearsLeft);
     return yearsLeft;
   }
 
